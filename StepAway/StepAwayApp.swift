f64eac6233428803942b171b778b5985e5445ea7
hrob2113 <robinhrdlickadeveloper@gmail.com>
2026-03-29 19:05:54 +0200

    Initial Commit

---
+++ StepAway/StepAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepAwayApp.swift
+//  StepAway
+//
+//  Created by Robin Hrdlička on 29.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct StepAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
